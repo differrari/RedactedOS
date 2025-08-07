51f4e4de5ec5c9de42c08d74815abf2bc5d31d49
Diego Ferrari <services@diferrari.com>
2025-08-07 00:00:00 +0000

    [DRIVER] disk module, optional

--- kernel/filesystem/disk.h
+++ kernel/filesystem/disk.h
@@ -5,12 +5,15 @@ extern "C" {
 #endif
 
 #include "types.h"
+#include "dev/driver_base.h"
 
 bool init_disk_device();
 void disk_verbose();
 
 void disk_write(const void *buffer, uint32_t sector, uint32_t count);
 void disk_read(void *buffer, uint32_t sector, uint32_t count);
+
+extern driver_module disk_module;
 
 #ifdef __cplusplus
 }
