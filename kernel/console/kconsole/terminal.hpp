045f223e8bb7f86a6b8f9abc54da14d0afe10b0c
Di Ferrari <git@diferrari.com>
2025-08-08 00:00:00 +0000

    [CONSOLE] removed unused function

--- kernel/console/kconsole/terminal.hpp
+++ kernel/console/kconsole/terminal.hpp
@@ -9,7 +9,6 @@ protected:
     void handle_input();
     void end_command();
     void run_command();
-    const char* seek_to(const char *string, char character);
 
     //TODO: proper commands
     void TMP_cat(const char *args);
