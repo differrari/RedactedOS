c878859ba83ed46cda600381ebc354a10fc9f452
Matthew Brush <mbrush@codebrainz.ca>
2025-08-10 22:50:51 -0700

    Use standard freestanding headers

--- shared/types.h
+++ shared/types.h
@@ -8,39 +8,16 @@
 extern "C" {
 #endif
 
-
-
-typedef unsigned int uint32_t;
-typedef long unsigned int size_t;
-typedef unsigned long uint64_t;
-typedef unsigned long uintptr_t;
-typedef unsigned short uint16_t;
-typedef unsigned char uint8_t;
-
-#define UINT64_MAX 0xFFFFFFFFFFFFFFFFULL
-#define UINT16_MAX 0xFFFF
-#define UINT32_MAX 0xFFFFFFFF
-
-typedef int int32_t;
-typedef long int64_t;
-typedef long intptr_t;
-typedef short int16_t;
-typedef char int8_t;
+// Note: only include freestanding headers here
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 typedef struct sizedptr {
     uintptr_t ptr;
     size_t size;
 } sizedptr;
 
-#define NULL 0
-
 #ifdef __cplusplus
-}
-#else
-
-typedef unsigned int bool;
-
-#define true 1
-#define false 0
-
+} // extern "C"
 #endif
