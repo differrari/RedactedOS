8e13bdcaea3334c677b35ca6f4028c2174212a11
Diego Ferrari <services@diferrari.com>
2025-08-25 00:00:00 +0000

    [DEFAULT_PROC] updated default process syntax

--- user/default_process.c
+++ user/default_process.c
@@ -10,16 +10,13 @@ void proc_func() {
     gpu_rect rect = (gpu_rect){{10,10},{size.width-20,size.height-20}};
     while (1) {
         keypress kp;
-        // printf("Print console test %f", (get_time()/1000.f));
-        while (read_key(&kp)){
+        printf("Print console test %f", (get_time()/1000.f));
+        if (read_key(&kp))
             if (kp.keys[0] == KEY_ESC)
                 halt(0);
-        }
         fb_clear(&ctx, 0xFFFFFFFF);
         fb_fill_rect(&ctx, rect.point.x, rect.point.y, rect.size.width, rect.size.height, 0xFF222233);
-        string s = string_from_literal("Print screen test");
-        fb_draw_string(&ctx, s.data, rect.point.x, rect.point.y, 2, 0xFFFFFFFF);
-        free(s.data,s.mem_length);
+        fb_draw_string(&ctx, "Print screen test", rect.point.x, rect.point.y, 2, 0xFFFFFFFF);
         commit_draw_ctx(&ctx);
     }
     halt(1);
