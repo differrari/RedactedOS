ccb4b27b40695617aa9cc1871f1c84d621f4c8fa
Diego Ferrari <services@diferrari.com>
2025-09-01 00:00:00 +0000

    [SYSCALL, TMP] switching the sp before entering the handler c function (currently using x1, which is probably not right)

--- kernel/process/syscall.c
+++ kernel/process/syscall.c
@@ -175,9 +175,6 @@ void sync_el0_handler_c(){
     save_return_address_interrupt();
 
     syscall_depth++;
-    
-    if (ksp > 0)
-        asm volatile ("mov sp, %0" :: "r"(ksp));
 
     process_t *proc = get_current_proc();
 
@@ -188,9 +185,6 @@ void sync_el0_handler_c(){
     asm volatile ("mrs %0, spsr_el1" : "=r"(spsr));
 
     uint64_t currentEL = (spsr >> 2) & 3;
-
-    uint64_t sp_el;
-    asm volatile ("mov %0, x11" : "=r"(sp_el));
     
     uint64_t esr;
     asm volatile ("mrs %0, esr_el1" : "=r"(esr));
