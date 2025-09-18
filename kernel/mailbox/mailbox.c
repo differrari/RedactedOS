095454d2249f71b446ef177576f5bcce30954662
Diego Ferrari <services@diferrari.com>
2025-09-18 00:00:00 +0000

    [MAILBOX] timeout

--- kernel/mailbox/mailbox.c
+++ kernel/mailbox/mailbox.c
@@ -1,5 +1,7 @@
 #include "mailbox.h"
 #include "memory/mmu.h"
+#include "console/serial/uart.h"
+#include "async.h"
 
 void mailbox_init(){
     register_device_memory(MAILBOX_BASE, MAILBOX_BASE);
@@ -7,15 +9,24 @@ void mailbox_init(){
 
 int mailbox_call(volatile uint32_t* mbox, uint8_t channel) {
     uint32_t addr = ((uint32_t)(uintptr_t)mbox) & ~0xF;
-
-    while (MBOX_STATUS & MBOX_FULL);
+    if (!wait(MBOX_STATUS, MBOX_FULL, false, 2000)){
+        uart_puts("[MAILBOX] could not find free mailbox slot\n");
+        return false;
+    }
     MBOX_WRITE = addr | (channel & 0xF);
 
     while (1) {
-        while (MBOX_STATUS & MBOX_EMPTY);
+        if (!wait(MBOX_STATUS, MBOX_EMPTY, false, 2000)){
+            uart_puts("[MAILBOX] No response received\n");
+            return false;
+        }
         uint32_t resp = MBOX_READ;
         if ((resp & 0xF) == channel && (resp & ~0xF) == addr)
             return mbox[1] == 0x80000000;
-        else return false;
+        else {
+            uart_puts("[MAILBOX] Wrong respnonse ");
+            uart_puthex(resp);
+            return false;
+        }
     }
 }
--- kernel/mailbox/mailbox.h
+++ kernel/mailbox/mailbox.h
@@ -4,7 +4,7 @@
 #include "hw/hw.h"
 
 #define MBOX_READ       (*(volatile uint32_t*)(MAILBOX_BASE + 0x00))
-#define MBOX_STATUS     (*(volatile uint32_t*)(MAILBOX_BASE + 0x18))
+#define MBOX_STATUS     ((uint32_t*)(MAILBOX_BASE + 0x18))
 #define MBOX_WRITE      (*(volatile uint32_t*)(MAILBOX_BASE + 0x20))
 
 #define MBOX_FULL       0x80000000
