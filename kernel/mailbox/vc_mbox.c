112bdc7380aae8b0b6522d384826e1e2b9411841
CodeAnarchist <leonardo300506@gmail.com>
2025-10-13 11:39:38 +0200

    [VIDEOCORE]restore old code, fix and improve mbox helper

--- kernel/mailbox/vc_mbox.c
+++ kernel/mailbox/vc_mbox.c
@@ -1,120 +1,163 @@
 #include "vc_mbox.h"
 #include "mailbox/mailbox.h"
 
-bool mbox_parse_tag(volatile uint32_t* buf, uint32_t tag, uint32_t* out, uint32_t words) {
-    uint32_t i = 2;
-    for (;;) {
-        uint32_t t = buf[i];
-        if (t == 0) break;
+#define MBOX_CH_PROP 8
+#define TAG_ALLOCATE_BUFFER 0x00040001
+#define TAG_SET_PHYS_WH 0x00048003
+#define TAG_GET_PHYS_WH 0x00040003
+#define TAG_SET_VIRT_WH 0x00048004
+#define TAG_GET_VIRT_WH 0x00040004
+#define TAG_SET_DEPTH 0x00048005
+#define TAG_GET_DEPTH 0x00040005
+#define TAG_SET_PIXEL_ORDER 0x00048006
+#define TAG_GET_PITCH 0x00040008
+#define TAG_SET_VIRT_OFFSET 0x00048009
 
-        uint32_t sz = buf[i + 1];
-        uint32_t len = buf[i + 2];
-        if (t == tag) {
-            uint32_t to_copy = len >> 2;
-            if (to_copy > words) to_copy = words;
-            for (uint32_t j = 0; j < to_copy; ++j) out[j] = buf[i + 3 + j];
+#define REQ_CODE 0x00000000
+#define RESP_OK 0x80000000
+#define END_TAG 0x00000000
+
+static volatile uint32_t mbox_buf[128] __attribute__((aligned(16)));
+
+bool mbox_parse_tag(volatile uint32_t* buf, uint32_t tag, uint32_t* out, uint32_t words) {
+    uint32_t idx = 2, total = buf[0] / 4;
+    while (idx + 3 < total)
+    {
+        uint32_t t = buf[idx + 0];
+        uint32_t vlen = buf[idx + 1] / 4;
+        uint32_t ro = buf[idx + 2];
+        if (t == END_TAG) break;
+        if (t == tag && (ro & RESP_OK))
+        {
+            if (words > vlen) return false;
+            for (uint32_t i = 0; i < words; ++i) out[i] = buf[idx + 3 + i];
 
             return true;
         }
-        i += 3 + ((sz + 3) >> 2);
+        idx += 3 + vlen;
     }
     return false;
 }
 
 bool mbox_query_modes(uint32_t* phys_w, uint32_t* phys_h, uint32_t* virt_w, uint32_t* virt_h, uint32_t* depth, uint32_t* pitch, uint32_t pixel_format) {
-    static volatile uint32_t mb[64] __attribute__((aligned(16)));
-    uint32_t words = 2 + 3 + 3 + 3 + 3 + 3 + 1;
-    uint32_t bytes = words << 2;
-    uint32_t aligned = (bytes + 15u) & ~15u;
+    volatile uint32_t *b = mbox_buf;
+    uint32_t i = 2;
 
-    mb[0] = aligned;
-    mb[1] = 0;
+    b[0] = 0;
+    b[1] = REQ_CODE;
 
-    mb[2] = MBOX_VC_PHYS_SIZE_TAG;
-    mb[3] = 8;
-    mb[4] = 0;
-    mb[5] = 0;
-    mb[6] = 0;
+    uint32_t pixel_order;
+    switch (pixel_format) {
+    case (uint32_t)('X') | ('R' << 8) | ('2' << 16) | ('4' << 24):
+    case (uint32_t)('A') | ('R' << 8) | ('2' << 16) | ('4' << 24):
+    case (uint32_t)('R') | ('G' << 8) | ('2' << 16) | ('4' << 24):
+        pixel_order = 0;
+        break;
+    case (uint32_t)('X') | ('B' << 8) | ('2' << 16) | ('4' << 24):
+    case (uint32_t)('A') | ('B' << 8) | ('2' << 16) | ('4' << 24):
+    case (uint32_t)('B') | ('G' << 8) | ('2' << 16) | ('4' << 24):
+        pixel_order = 1;
+        break;
+    default:
+        pixel_order = 0;
+        break;
+    }
 
-    mb[7] = MBOX_VC_VIRT_SIZE_TAG;
-    mb[8] = 8;
-    mb[9] = 0;
-    mb[10] = 0;
-    mb[11] = 0;
+    b[i++] = TAG_SET_PIXEL_ORDER;
+    b[i++] = 4;
+    b[i++] = REQ_CODE;
+    b[i++] = pixel_order;
 
-    mb[12] = MBOX_VC_DEPTH_TAG | MBOX_SET_VALUE;
-    mb[13] = 4;
-    mb[14] = 4;
-    mb[15] = 32;
+    b[i++] = TAG_SET_DEPTH;
+    b[i++] = 4;
+    b[i++] = REQ_CODE;
+    b[i++] = 32;
 
-    mb[16] = MBOX_VC_PITCH_TAG;
-    mb[17] = 4;
-    mb[18] = 0;
-    mb[19] = 0;
+    b[i++] = TAG_GET_PHYS_WH;
+    b[i++] = 8;
+    b[i++] = REQ_CODE;
+    b[i++] = 0;
+    b[i++] = 0;
+    b[i++] = TAG_GET_VIRT_WH;
+    b[i++] = 8;
+    b[i++] = REQ_CODE;
+    b[i++] = 0;
+    b[i++] = 0;
+    b[i++] = TAG_GET_DEPTH;
+    b[i++] = 4;
+    b[i++] = REQ_CODE;
+    b[i++] = 0;
+    b[i++] = TAG_GET_PITCH;
+    b[i++] = 4;
+    b[i++] = REQ_CODE;
+    b[i++] = 0;
+    b[i++] = END_TAG;
+    b[0] = i * 4;
+    b[1] = REQ_CODE;
+    if (!mailbox_call(b, MBOX_CH_PROP)) return false;
+    if (!(b[1] & RESP_OK)) return false;
 
-    mb[20] = MBOX_VC_FORMAT_TAG | MBOX_SET_VALUE;
-    mb[21] = 4;
-    mb[22] = 4;
-    mb[23] = pixel_format;
+    uint32_t tmp[2];
+    if (!mbox_parse_tag(b, TAG_GET_PHYS_WH, tmp, 2)) return false;
+    *phys_w = tmp[0];
+    *phys_h = tmp[1];
 
-    mb[24] = 0;
+    if (!mbox_parse_tag(b, TAG_GET_VIRT_WH, tmp, 2)) return false;
+    *virt_w = tmp[0];
+    *virt_h = tmp[1];
 
-    if (!mailbox_call(mb, 8)) return false;
-
-    uint32_t tmp2[2];
-    if (!mbox_parse_tag(mb, MBOX_VC_PHYS_SIZE_TAG, tmp2, 2)) return false;
-    *phys_w = tmp2[0];
-    *phys_h = tmp2[1];
-
-    if (!mbox_parse_tag(mb, MBOX_VC_VIRT_SIZE_TAG, tmp2, 2)) return false;
-    *virt_w = tmp2[0];
-    *virt_h = tmp2[1];
-
-    uint32_t tmp1[1];
-    if (!mbox_parse_tag(mb, MBOX_VC_DEPTH_TAG, tmp1, 1)) return false;
-    *depth = tmp1[0];
-
-    if (!mbox_parse_tag(mb, MBOX_VC_PITCH_TAG, tmp1, 1)) return false;
-    *pitch = tmp1[0];
-
+    if (!mbox_parse_tag(b, TAG_GET_DEPTH, tmp, 1)) return false;
+    *depth = tmp[0];
+    if (!mbox_parse_tag(b, TAG_GET_PITCH, tmp, 1)) return false;
+    *pitch = tmp[0];
     return true;
 }
 
-bool mbox_alloc_fb(uint32_t virt_w, uint32_t virt_h, uint32_t* fb_addr, uint32_t* fb_size) {
-    static volatile uint32_t mb[64] __attribute__((aligned(16)));
-    uint32_t words = 2 + 3 + 3 + 1;
-    uint32_t bytes = words << 2;
-    uint32_t aligned = (bytes + 15u) & ~15u;
+bool mbox_alloc_fb(uint32_t virt_w, uint32_t virt_h, uint32_t* fb_bus_addr, uint32_t* fb_size) {
+    volatile uint32_t *b = mbox_buf;
+    uint32_t i = 2;
 
-    mb[0] = aligned;
-    mb[1] = 0;
+    b[0] = 0;
+    b[1] = REQ_CODE;
 
-    mb[2] = MBOX_VC_VIRT_SIZE_TAG | MBOX_SET_VALUE;
-    mb[3] = 8;
-    mb[4] = 8;
-    mb[5] = virt_w;
-    mb[6] = virt_h;
+    b[i++] = TAG_SET_VIRT_WH;
+    b[i++] = 8;
+    b[i++] = REQ_CODE;
+    b[i++] = virt_w;
+    b[i++] = virt_h;
 
-    mb[7] = MBOX_VC_FRAMEBUFFER_TAG;
-    mb[8] = 8;
-    mb[9] = 0;
-    mb[10] = 16;
-    mb[11] = 0;
+    b[i++] = TAG_ALLOCATE_BUFFER;
+    b[i++] = 8;
+    b[i++] = REQ_CODE;
+    b[i++] = 16;
+    b[i++] = 0;
 
-    mb[12] = 0;
-
-    if (!mailbox_call(mb, 8)) return false;
-
-    uint32_t tmp2[2];
-    if (!mbox_parse_tag(mb, MBOX_VC_FRAMEBUFFER_TAG, tmp2, 2)) return false;
-    *fb_addr = tmp2[0];
-    *fb_size = tmp2[1];
-
+    b[i++] = END_TAG;
+    b[0] = i * 4;
+    b[1] = REQ_CODE;
+    if (!mailbox_call(b, MBOX_CH_PROP)) return false;
+    if (!(b[1] & RESP_OK)) return false;
+    uint32_t tmp[2];
+    if (!mbox_parse_tag(b, TAG_ALLOCATE_BUFFER, tmp, 2)) return false;
+    *fb_bus_addr = tmp[0];
+    *fb_size = tmp[1];
     return true;
 }
 
 bool mbox_set_offset(uint32_t yoff) {
-    static volatile uint32_t mb[16] __attribute__((aligned(16))) = {30 * 4, 0, MBOX_VC_OFFSET_TAG | MBOX_SET_VALUE, 8, 8, 0, 0, 0,};
-    mb[6] = yoff;
-    return mailbox_call(mb, 8);
+    volatile uint32_t *b = mbox_buf;
+    uint32_t i = 2;
+    b[0] = 0;
+    b[1] = REQ_CODE;
+    b[i++] = TAG_SET_VIRT_OFFSET;
+    b[i++] = 8;
+    b[i++] = REQ_CODE;
+    b[i++] = 0;
+    b[i++] = yoff;
+    b[i++] = END_TAG;
+    b[0] = i * 4;
+    b[1] = REQ_CODE;
+    if (!mailbox_call(b, MBOX_CH_PROP)) return false;
+    if (!(b[1] & RESP_OK)) return false;
+    return true;
 }
