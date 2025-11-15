5ca04247b508e3bb15aa4fb9ab6697d2d8fcf279
dif <git@diferrari.com>
2025-11-15 00:00:00 +0000

    [TESTS] use after free test

--- kernel/tests/allocation/alloc_tests.c
+++ kernel/tests/allocation/alloc_tests.c
@@ -1,7 +1,14 @@
 #include "alloc_tests.h"
-#include "console/kio.h"
+#include "tests/test.h"
+
+bool test_after_free(){
+    uint64_t *a = malloc(64);
+    a[3] = 12345678;
+    free(a,64);
+    assert_eq(a[3], 0xDEADBEEFDEADBEEF, "Use after free failed: %x",a[3]);
+    return true;
+}
 
 bool alloc_tests(){
-    kprintf("Running tests for allocation");
-    return true;
+    return test_after_free();
 }
