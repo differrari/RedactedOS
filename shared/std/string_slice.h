53173336320fbec190416ae76c451f7a76da2613
di <git@diferrari.com>
2026-01-11 00:00:00 +0000

    [SLICE] slice comparison

--- shared/std/string_slice.h
+++ shared/std/string_slice.h
@@ -11,6 +11,14 @@ static inline string string_from_slice(string_slice slice){
     return string_from_literal_length(slice.data, slice.length);
 }
 
-static inline bool slice_lit_match(string_slice sl, const char *lit){
-    return (size_t)strstart(sl.data, lit) == sl.length;
+static inline bool slices_equal(string_slice sl1, string_slice sl2, bool case_insensitive){
+    if (sl1.length != sl2.length) return false;
+    for (size_t i = 0; i < sl1.length; i++)
+        if (sl1.data[i] != sl2.data[i] && (!case_insensitive || tolower(sl1.data[i]) != tolower(sl2.data[i]))) return false;
+    return true;
 }
+
+static inline bool slice_lit_match(string_slice sl, const char *lit, bool case_insensitive){
+    return slices_equal(sl, make_string_slice(lit,0,strlen(lit)), case_insensitive);
+}
+
