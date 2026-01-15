2a968dd36fea67e777cbf8d4f38745e6ef875e50
di <git@diferrari.com>
2026-01-15 00:00:00 +0000

    [WM] fix for window management navigation

--- kernel/kernel_processes/windows/wincomp.c
+++ kernel/kernel_processes/windows/wincomp.c
@@ -2,15 +2,15 @@
 #include "ui/graphic_types.h"
 #include "graph/tres.h"
 #include "graph/graphics.h"
-#include "console/kio.h"
 #include "math/math.h"
+#include "console/kio.h"
 
 int_point current_win_offset = {};
 
 void new_managed_window(){
     draw_ctx *cur = gpu_get_ctx();
     create_window(10 - current_win_offset.x, 10 - current_win_offset.y, cur->width - 20, cur->height - 20);
-    // global_win_offset.x = current_win_offset.x;
+    global_win_offset.x = current_win_offset.x;
     current_win_offset.x -= cur->width;
 }
 
@@ -22,8 +22,8 @@ void switch_focus(int8_t x, int8_t y){
     global_win_offset.x += cur->width * x;
     global_win_offset.y += cur->height * y;
     
-    global_win_offset.x = (((int32_t)(global_win_offset.x/cur->width))*cur->width);
-    global_win_offset.y = (((int32_t)(global_win_offset.y/cur->height))*cur->height);
+    global_win_offset.x = (((int32_t)(global_win_offset.x/(int32_t)cur->width))*cur->width);
+    global_win_offset.y = (((int32_t)(global_win_offset.y/(int32_t)cur->height))*cur->height);
     
     dirty_windows = true;
 }
